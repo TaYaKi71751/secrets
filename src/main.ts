0bc90de76de861ee57111efe627351d3a93eab35
TaYaKi71751 <shantian.tw@gmail.com>
2022-03-15 09:40:52 +0900

    Create *.ts

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import * as core from '@actions/core';
+async function run (): Promise<void> {
+	try {
+		const secrets: any = JSON.parse(core.getInput('secrets'));
+		const name: string = core.getInput('name');
+		let exists = false;
+		let value: string = '';
+		Object.entries(secrets).forEach(([k, v]:any) => {
+			const e = `${k.toLowerCase()}` == `${name.toLowerCase()}`;
+			exists = exists || e;
+			value = value || v;
+			if (e) {
+				core.setOutput('exists', exists);
+				core.setOutput('value', value);
+			}
+		});
+		if (!exists) {
+			core.warning(`secrets.${name} is not exist`);
+		}
+	} catch (error) {
+		if (error instanceof Error) core.setFailed(error.message);
+	}
+}
+
+run();
